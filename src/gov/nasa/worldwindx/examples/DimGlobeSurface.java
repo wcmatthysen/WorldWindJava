8a2d2c5f4f407c9ffbda8f8d35656813ee566bfe
Wiehann Matthysen <wcmatthysen@gmail.com>
2021-01-21 16:09:23 +0200

    DimGlobeSurface uses SurfaceColorLayer.
    Modified the DimGlobeSurface example to make use of the
    SurfaceColorLayer class instead of manually creating all of the
    components to achieve the desired result.

--- src/gov/nasa/worldwindx/examples/DimGlobeSurface.java
+++ src/gov/nasa/worldwindx/examples/DimGlobeSurface.java
@@ -30,7 +30,7 @@ package gov.nasa.worldwindx.examples;
 
 import gov.nasa.worldwind.geom.Sector;
 import gov.nasa.worldwind.layers.RenderableLayer;
-import gov.nasa.worldwind.render.SurfaceImage;
+import gov.nasa.worldwind.layers.SurfaceColorLayer;
 
 import javax.swing.*;
 import javax.swing.border.*;
@@ -39,12 +39,14 @@ import java.awt.*;
 import java.awt.image.*;
 
 /**
- * Shows how to add a layer over the globe's surface imagery to simulate dimming the surface. The technique is very
- * simple: just create a {@link SurfaceImage}, apply it to the full globe, and use its opacity to control the amount
- * of dimming. This example uses a black surface image, but any color could be used.
+ * Shows how to add a layer over the globe's surface imagery to simulate dimming
+ * the surface. The technique is very simple: just create a {@link SurfaceColorLayer},
+ * and use its opacity to control the amount of dimming. This example uses a
+ * black surface image, but any color could be used.
  *
- * Note that this does not provide a filtering effect -- enhancing or blocking specific colors. For that
- * <code>SurfaceImage</code> would need blending controls, but it doesn't have them.
+ * Note that this does not provide a filtering effect -- enhancing or blocking
+ * specific colors. For that <code>SurfaceColorLayer</code> would need blending
+ * controls, but it doesn't have them.
  *
  * @author tag
  * @version $Id: DimGlobeSurface.java 2109 2014-06-30 16:52:38Z tgaskins $
@@ -53,23 +55,18 @@ public class DimGlobeSurface extends ApplicationTemplate
 {
     public static class AppFrame extends ApplicationTemplate.AppFrame
     {
-        protected SurfaceImage surfaceImage;
+        protected SurfaceColorLayer layer;
         protected JSlider opacitySlider;
 
         public AppFrame()
         {
             super(true, true, false);
 
-            // Create a surface image covering the full globe and set its initial opacity.
-
-            this.surfaceImage = new SurfaceImage(this.makeFilterImage(), Sector.FULL_SPHERE);
-            this.surfaceImage.setOpacity(0.10);
-
-            RenderableLayer layer = new RenderableLayer();
-            layer.setName("Surface Dimmer");
-            layer.setPickEnabled(false);
-
-            layer.addRenderable(surfaceImage);
+            // Create a surface color layer covering the full globe and set its initial opacity.
+            this.layer = new SurfaceColorLayer(Color.BLACK);
+            this.layer.setOpacity(0.10);
+            this.layer.setPickEnabled(false);
+            this.layer.setName("Surface Dimmer");
 
             ApplicationTemplate.insertBeforePlacenames(this.getWwd(), layer);
 
@@ -83,31 +80,16 @@ public class DimGlobeSurface extends ApplicationTemplate
             this.getControlPanel().add(opacityPanel, BorderLayout.SOUTH);
         }
 
-        protected BufferedImage makeFilterImage()
-        {
-            // A very small image can be used because it's all the same color.
-            BufferedImage image = new BufferedImage(4, 4, BufferedImage.TYPE_INT_ARGB);
-
-            Graphics2D g = (Graphics2D) image.getGraphics();
-
-            g.setColor(new Color(0f, 0f, 0f, 1f)); // black, but any color could be used
-            g.fillRect(0, 0, image.getWidth(), image.getHeight());
-
-            g.dispose();
-
-            return image;
-        }
-
         protected void makeOpacitySlider()
         {
-            this.opacitySlider = new JSlider(JSlider.HORIZONTAL, 0, 100, (int) (this.surfaceImage.getOpacity() * 100));
+            this.opacitySlider = new JSlider(JSlider.HORIZONTAL, 0, 100, (int) (this.layer.getOpacity() * 100));
             this.opacitySlider.setToolTipText("Filter opacity");
             this.opacitySlider.addChangeListener(new ChangeListener()
             {
                 public void stateChanged(ChangeEvent event)
                 {
                     double value = opacitySlider.getValue();
-                    surfaceImage.setOpacity(value / 100);
+                    layer.setOpacity(value / 100);
                     getWwd().redraw();
                 }
             });
