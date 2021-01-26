1723580518d9121509458b2f46c0bb7da9d5caed
Wiehann Matthysen <wcmatthysen@gmail.com>
2021-01-26 16:05:13 +0200

    GeographicText x and y offsets.
    - Modified the GeographicText interface by adding getter / setter
      methods to specify an offset.
    - Implemented the abovementioned getter / setter methods in the
      UserFacingText class.
    - Modified both the GeographicTextRenderer and DeclutteringTextRenderer
      classes by making use of the abovementioned offset property to offset
      the text of a GeographicText object relative to its position.

--- src/gov/nasa/worldwind/render/DeclutteringTextRenderer.java
+++ src/gov/nasa/worldwind/render/DeclutteringTextRenderer.java
@@ -28,6 +28,7 @@
 
 package gov.nasa.worldwind.render;
 
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.geom.*;
 import gov.nasa.worldwind.globes.Globe2D;
 import gov.nasa.worldwind.terrain.SectorGeometryList;
@@ -268,16 +269,20 @@ public class DeclutteringTextRenderer
                     color = DEFAULT_COLOR;
                 color = this.applyOpacity(color, opacity);
 
+                Offset offset = geographicText.getOffset();
+                float xOffset = offset.getX().floatValue();
+                float yOffset = offset.getY().floatValue();
+
                 Color background = geographicText.getBackgroundColor();
                 if (background != null)
                 {
                     background = this.applyOpacity(background, opacity);
                     textRenderer.setColor(background);
-                    textRenderer.draw3D(charSequence, drawPoint.x + 1, drawPoint.y - 1, 0, 1);
+                    textRenderer.draw3D(charSequence, drawPoint.x + xOffset + 1, drawPoint.y + yOffset - 1, 0, 1);
                 }
 
                 textRenderer.setColor(color);
-                textRenderer.draw3D(charSequence, drawPoint.x, drawPoint.y, 0, 1);
+                textRenderer.draw3D(charSequence, drawPoint.x + xOffset, drawPoint.y + yOffset, 0, 1);
                 textRenderer.flush();
 
                 if (scale != 1d)
@@ -378,8 +383,9 @@ public class DeclutteringTextRenderer
 
             Rectangle2D textBound = textRenderer.getBounds(charSequence);
             double x = screenPoint.x - textBound.getWidth() / 2d;
+            Offset offset = geographicText.getOffset();
             Rectangle2D bounds = new Rectangle2D.Float();
-            bounds.setRect(x, screenPoint.y, textBound.getWidth(), textBound.getHeight());
+            bounds.setRect(x + offset.x, screenPoint.y + offset.y, textBound.getWidth(), textBound.getHeight());
 
             return bounds;
         }
